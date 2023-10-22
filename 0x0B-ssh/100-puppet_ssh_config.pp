dcc97b93f46f5d43ff7af5a248972c39dc639d10
fatimazahraben-alla <fatisafae33@gmail.com>
2023-10-22 09:49:39 +0000

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 # Puppet script to create ssh config file
 file { '/etc/ssh/ssh_config':
-  ensure => 'present',
+  ensure => present,
 
-  line   => '
+content   => "
+            #SSH Client Configuration
             host*
-            PasswordAuthentication no
             IdentityFile ~/.ssh/school
-            ',
+            PasswordAuthentication no
+            ",
 }
